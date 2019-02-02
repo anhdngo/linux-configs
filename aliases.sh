b5307993e4ab330cb8afaf4aa56560635ea42992
Anh Ngo <adn057@ucsd.edu>
2019-02-02 01:10:52 -0800

    add alias file

---
+++ aliases.sh
@@ -0,0 +1,11 @@
+# Ease of Use
+alias sl="ls"
+alias l="ls"
+alias ll="ls -la"
+alias reload="exec $SHELL -l"
+
+# Shortcuts
+alias dt="cd ~/Desktop"
+alias dl="cd ~/Downloads"
+alias doc="cd ~/Documents"
+alias ws="cd ~/workspace"
