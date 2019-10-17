541fe42dbe52b9345bcfc814cefb3d3f074fdabf
Anh Ngo <adn057@ucsd.edu>
2019-10-17 11:26:30 -0700

    improve lockscreen

--- assets/lock.sh
+++ assets/lock.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-TMPBG=/tmp/screen.png
-LOCK=$HOME/.linux_configs/assets/lock.png
-RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
- 
-ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
-i3lock -i $TMPBG
-rm $TMPBG
+
+# Dependencies:
+# ffmpeg
+# i3lock-color-git
+
+IMAGE=/tmp/i3lock.png
+TEXT="Locked"
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+LOCK=$SCRIPTPATH/lock.png
+RES=$(xrandr --current | grep '*' | uniq | awk '{print $1}')
+#This is a bit cheaty
+BOXSIZE=$(expr $(echo $RES | cut -d 'x' -f1) / 2)
+
+if [[ $1 != "" ]]; then
+    TEXT=$1
+fi
+
+ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5,drawtext=text=$TEXT:fontcolor='#ffffffe0':fontsize=40:x=(w-tw)/2:y=(h/PHI)+th:shadowcolor='#1d1f21d0':shadowx=2:shadowy=2:box=1:boxcolor=#1a1b22@0.4:boxborderw=$BOXSIZE,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2,eq=brightness=-0.04" -vframes 1 $IMAGE
+
+i3lock -i $IMAGE
+
+rm $IMAGE
